0fc953787c09950a17048ff94304af28cd193fd6
syahrudinsmj <syahrudinsmj@gmail.com>
2025-07-16 09:56:42 +0700

    update

--- src/views/templates/PageTemplate.js
+++ src/views/templates/PageTemplate.js
@@ -41,6 +41,8 @@ const PageTemplate = (data) => `
   </nav>
   <main>
     {{content}}
+    
+    <a href="https://info.flagcounter.com/fyvX"><img src="https://s01.flagcounter.com/count/fyvX/bg_FFFFFF/txt_000000/border_FFFFFF/columns_8/maxflags_20/viewers_0/labels_1/pageviews_1/flags_0/percent_0/" alt="Flag Counter" border="0"></a>
   </main>
 `;
 
